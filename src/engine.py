4e9d58623261f6b8cee616f2867b49b4222341a7
samTime101 <srijanregmi44@gmail.com>
2026-04-17 13:35:02 +0000

    init: is_empty method

--- src/engine.py
+++ src/engine.py
@@ -12,11 +12,14 @@ class Engine:
         self.goats_captured = 0
         self.winner = None
 
+    def is_empty(self, pos):
+        return self.board[pos] == 0
+
     def valid_normal_move(self, current, target):
-        return target in N[current] and self.board[target] == 0
+        return target in N[current] and self.is_empty(target)
 
     def valid_jump_move(self, current, target):
         if (current, target) not in J:
             return False
         midpoint = J[(current, target)]
-        return self.board[midpoint] == 1 and self.board[target] == 0
+        return self.board[midpoint] == 1 and self.is_empty(target)
